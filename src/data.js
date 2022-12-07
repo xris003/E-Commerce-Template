3c952fdaaea7253d4b5165d2ff4f6d30ed0c0dba
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2022-12-07 12:15:46 +0100

    Fixed Codes

--- src/data.js
+++ src/data.js
@@ -3,26 +3,26 @@ let shopItemsData = [{
 	name:"Miss Gift XYZ",
 	price: 65,
 	desc: "Ambassador and CEO of Gift Collection",
-	img:"img/DesignC.jpg"
+	img:"img/15.jpg"
 },
 {
 	id: "unitTwo",
 	name:"Miss Kossy XYZ",
 	price: 45,
 	desc: "Ambassador and CEO of Kossy Collection",
-	img:"img/DesignB.jpg"
+	img:"img/14.jpg"
 },
 {
 	id: "unitThree",
 	name:"Ms Daniel XYZ",
 	price: 85,
 	desc: "Ambassador and CEO of Dhaniele Couture",
-	img:"img/DesignA.jpg"
+	img:"img/13.jpg"
 },
 {
 	id: "unitFour",
 	name:"Ms OG XYZ",
 	price: 75,
 	desc: "Ambassador and CEO of OG Kollection",
-	img:"img/gabriel1.jpg"
+	img:"img/12.jpg"
 }];
